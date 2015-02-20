c4a960a0aa38d20237625684cf53cb1caadc2998
hbfhaapy <huopo125@163.com>
2015-02-20 09:53:28 +0800

    fix

--- app/controllers/api/v1/user_count_controller.rb
+++ app/controllers/api/v1/user_count_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::UserCountController < ApplicationController
+class Api::V1::UserCountController < ActionController::Base
   def all
     @loginers = $redis.zcount :login_users, 5.minutes.ago.to_i, Time.now.to_i
     @guests = $redis.zcount :guest_users, 5.minutes.ago.to_i, Time.now.to_i
